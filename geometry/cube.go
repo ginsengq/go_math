3939be43a8768e23640dd690b98f88c4ce2bbf78
Diana <di@di.di>
2024-08-29 01:10:49 +0500

    zero edge is not allowed

--- geometry/cube.go
+++ geometry/cube.go
@@ -1,5 +1,24 @@
 package geometry
 
-func CubeVolume(n int) int {
-	return n * n * n
+import "errors"
+
+func CubeVolume(n int) (int, error) {
+	if n != 0 {
+		return n * n * n, nil
+	} else {
+		return 0, errors.New("Zero length not allowed")
+	}
 }
+
+//semantic versioning - universally accepted format to tack release of packages,
+//modules directly to version string
+
+//vMAJOR.MINOR.PATCH
+//v1.2.3
+//major - change when you make incompatible changes
+//minor - when add functionality in backward minor
+//patch - when make backward compatible fixes
+
+//git tag -a v1.0.0 -m "initial release"
+//git push origin master --tags
+//we published module with its corresponding tag/version
